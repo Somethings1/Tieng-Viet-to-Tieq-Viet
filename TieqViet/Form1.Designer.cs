572f3e05e43a007e3eedbac15b842a787406aa73
Nguyen Trung Long <somethings.ttt@gmail.com>
2018-02-14 16:18:41 +0700

    README.md

---
+++ TieqViet/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TieqViet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBox1.Location = new System.Drawing.Point(12, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(674, 159);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBox2.Location = new System.Drawing.Point(12, 263);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(674, 159);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tiếng Việt: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tiếq Việt: ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(615, 429);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(71, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "What is this? ";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(698, 451);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Convert \"Tiếng Việt\" to \"Tiếq Việt\"";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ TieqViet/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TieqViet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.c-sharpcorner.com");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s = this.textBox1.Text, result = "", ss = "";
+            s += " ";
+            while(s != "")
+            {
+                ss = s.Substring(0, s.IndexOf(" "));
+                s = s.Substring(s.IndexOf(" ") + 1);
+                if(ss.Contains("ch") || ss.Contains("Ch"))
+                {
+                    ss = ss.Replace("ch", "c");
+                    ss = ss.Replace("Ch", "C");
+                }
+                else
+                {
+                    ss = ss.Replace("c", "k");
+                    ss = ss.Replace("C", "K");
+                }
+                ss = ss.Replace("R", "Z");
+                ss = ss.Replace("r", "z");
+                ss = ss.Replace("D", "Z");
+                ss = ss.Replace("d", "z");
+                ss = ss.Replace("Gi", "Z");
+                ss = ss.Replace("gi", "z");
+                ss = ss.Replace("Tr", "C");
+                ss = ss.Replace("tr", "c");
+                ss = ss.Replace("Đ", "D");
+                ss = ss.Replace("đ", "d");
+                ss = ss.Replace("Gh", "G");
+                ss = ss.Replace("gh", "g");
+                ss = ss.Replace("Ph", "F");
+                ss = ss.Replace("ph", "f");
+                ss = ss.Replace("Q", "K");
+                ss = ss.Replace("q", "k");
+                ss = ss.Replace("Ngh", "Q");
+                ss = ss.Replace("ngh", "q");
+                ss = ss.Replace("Ng", "Q");
+                ss = ss.Replace("ng", "q");
+                ss = ss.Replace("Kh", "X");
+                ss = ss.Replace("kh", "x");
+                ss = ss.Replace("Th", "w");
+                ss = ss.Replace("th", "w");
+                ss = ss.Replace("Nh", "N'");
+                ss = ss.Replace("nh", "n'");
+                result += ss + " ";
+            }
+            this.textBox2.Text = result;
+        }
+    }
+}
